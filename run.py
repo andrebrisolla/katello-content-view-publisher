0946d5d6736a4032bfbc018526dfb6adc2f40712
andre.brisolla <andre.brisolla@stone.local@jump-v-lx-1p01.stone.local>
2022-05-18 15:39:14 +0000

    Testing yaml

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 import yaml
 import os
 import configparser
+import sys
+import argparse
 
 class Katello:
 
@@ -12,7 +14,7 @@ class Katello:
             yml = yaml.safe_load(file_data)
             return yml
         except Exception as err:
-            print(str(err))
+            raise str(err)
     
     def load_conf(self):
         ''' load configs '''
@@ -25,13 +27,32 @@ class Katello:
                 'environment' : environment
             }
         except Exception as err:
-            print(str(err))
+            raise str(err)
 
     def __init__(self):
         self.fullpath = os.path.abspath(os.path.dirname(__file__))
         self.product = self.load_yaml()
         self.configs = self.load_conf()
-        
 
-kt = Katello()
-print(kt.configs)
+    def verify(self):
+        print('verifica')
+    
+    def create(self):
+        print('cria')
+        
+if __name__ == '__main__':
+    parse = argparse.ArgumentParser(description="Create a Katello Content View")
+    parse.add_argument('--verify', help='Verify if necessary a new version of a Content View.', action='store_true' )
+    parse.add_argument('--create', help='Create a new version of a Content View if necessary.', action='store_true')
+    arguments = parse.parse_args()
+    args = vars(arguments)
+    if (args['verify'] and args['create']) or (not args['verify'] and not args['create']):
+        print('\nError: You need to specify one parameter.\n')
+        parse.print_help()
+        sys.exit(1)
+    elif args['verify']:
+        kt = Katello()
+        kt.verify()
+    elif args['create']:
+        kt = Katello()
+        kt.create()
