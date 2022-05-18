ef4051a4836a3ab7941b5f779ab2130e5aeb6de5
andre.brisolla <andre.brisolla@stone.local@jump-v-lx-1p01.stone.local>
2022-05-18 14:47:34 +0000

    Testing yaml

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+import yaml
+import os
+
+class Katello:
+
+    def load_yaml_data(self):
+        try:
+            file = open(f'{self.fullpath}', 'r').read()
+            print(file)
+        except Exception as err:
+            return str(err)
+
+    def __init__(self):
+        self.fullpath = os.path.abspath(os.path.dirname(__file__))
+        ''' load yaml data '''
+
+kt = Katello()
+kt.load_yaml_data()
