fee762f467798e432d066ae4a6a03aafc4c2a07f
andre.brisolla <andre.brisolla@stone.local@jump-v-lx-1p01.stone.local>
2022-05-18 14:53:49 +0000

    Testing yaml

--- run.py
+++ run.py
@@ -5,15 +5,18 @@ import os
 class Katello:
 
     def load_yaml_data(self):
+        ''' load yaml data '''
         try:
-            file = open(f'{self.fullpath}/products.yml', 'r').read()
-            print(file)
+            file_data = open(f'{self.fullpath}/products.yml', 'r').read()
+            yml = yaml.safe_load(file_data)
+            return yml
         except Exception as err:
             print(str(err))
 
     def __init__(self):
         self.fullpath = os.path.abspath(os.path.dirname(__file__))
-        ''' load yaml data '''
+        self.product = self.load_yaml_data()
+        
 
 kt = Katello()
-kt.load_yaml_data()
+print(kt.product)
