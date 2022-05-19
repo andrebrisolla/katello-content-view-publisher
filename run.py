868c34db416af1396e353d965d7a23526ea6b02f
andre.brisolla <andre.brisolla@stone.local@jump-v-lx-1p01.stone.local>
2022-05-19 20:03:51 +0000

    Remove config.ini, testing hammer cli

--- run.py
+++ run.py
@@ -8,16 +8,28 @@ import subprocess as sb
 import json
 from datetime import datetime, timedelta
 import re
-
+'''
+    Author: Andre Brisolla
+    Date: May 18, 2022
+    Goal: This script will check if is necessary 
+    to publish a new Version of a Content View on Katello!
+    Version: 1.0
+'''
 class Katello:
     
     def __init__(self, **kwargs):
+        '''
+            Set some class attributes
+        '''
         self.fullpath = os.path.abspath(os.path.dirname(__file__))
         self.products = self.load_yaml()
         self.env = kwargs['env']
         self.publish_new_version=kwargs['publish_new_version']
 
     def load_yaml(self):
+        '''
+            Load yaml data
+        '''
         try:
             file_data = open(f'{self.fullpath}/products.yml', 'r').read()
             yml = yaml.safe_load(file_data)
@@ -26,20 +38,26 @@ class Katello:
             raise str(err)
     
     def analyse_sync_date(self, **kwargs):
+        '''
+            Unfortunatly the 'hammer' command does not returns a timestamp of a sync,
+            it returns a string like: '"about 5 days",  "about 2 hours". So, 
+            we need to take this informations and turn it into a datetime object to compare with
+            ContentView datetimes and "Now" datetime.
+        '''
         try:
             moment = kwargs['moment']
             filter_1 = moment.replace('about','')
             filter_2 = re.sub('^\s+','', filter_1)
             time_key=''
             if re.search('day', filter_2):
-              time_key='days'
-              time_val = int(filter_2.split()[0])*24
+                time_key='days'
+                time_val = int(filter_2.split()[0])*24
             elif re.search('hour', filter_2):
-              time_key='hours'
-              time_val = int(filter_2.split()[0])
+                time_key='hours'
+                time_val = int(filter_2.split()[0])
             if re.search('month', filter_2):
-              time_key='days'
-              time_val = (30*int(filter_2.split()[0])*24)
+                time_key='days'
+                time_val = (30*int(filter_2.split()[0])*24)
             td = timedelta(hours=time_val)
             now = datetime.now()
             dt = now - td
@@ -48,6 +66,11 @@ class Katello:
             raise str(err)
 
     def get_repo_sync_info(self, **kwargs):
+        '''
+            This method receives a list of repositories ID`s.
+            It uses the command "hammer" to take 
+            informations about sync and then return the information.
+        '''
         try:
             ids = kwargs['repositories']
             info_repos = []
@@ -68,6 +91,10 @@ class Katello:
             raise str(err)
     
     def get_content_view_info(self):
+        '''
+            Get a list of Content View that is Published in Katello
+            It uses a organization-id 1 that is refenced to the Default Organization
+        '''
         try:
             cmd = ['hammer', 
                     '--output', 'json', 
@@ -82,19 +109,28 @@ class Katello:
             raise str(err)
 
     def publish_a_new_content_view(self, **kwargs):
-      try:
+        '''
+            It receives a Content View ID and publish a new Version of it.
+        '''
+        try:
         id = kwargs['id']
         cmd = ['hammer', 'content-view', 'publish', '--id', f'{id}', '--async']
         res = sb.run(cmd, stdout=sb.PIPE, stderr=sb.PIPE)
         if res.returncode == 0:
-          ret = res.stdout.decode('utf-8')
+            ret = res.stdout.decode('utf-8')
         else:
-          ret = res.stderr.decode('utf-8')
+            ret = res.stderr.decode('utf-8')
         print(f'   {ret}')
-      except Exception as err:
+        except Exception as err:
         raise str(err)
 
     def analyse_date(self, **kwargs):
+        '''
+            This method will iterate at a Content View data that was updated with
+            repository infos ( like repo last sync ) and will show if is necessary to
+            publish a new version of a Content View, if the parameter "--publish" was defined it will call the method
+            "publish_a_new_content_view" that will publish a new version.
+        '''
         try:
             data = kwargs['data']
             for cv in data:
@@ -106,11 +142,11 @@ class Katello:
                 compare_cv_repos = [ date_obj > x['last_sync'] for x in cv['sync_info'] ]
                 cv_needs_to_be_published = False in compare_cv_repos
                 if cv_needs_to_be_published:
-                  print(f' - Content View "{cv_name}" needs to be published! ')
-                  if self.publish_new_version:
-                    self.publish_a_new_content_view(id=cv_id)
+                    print(f' - Content View "{cv_name}" needs to be published! ')
+                    if self.publish_new_version:
+                        self.publish_a_new_content_view(id=cv_id)
                 else:
-                  print(f' - Content View "{cv_name}" don\'t needs to be published! ')
+                    print(f' - Content View "{cv_name}" don\'t needs to be published! ')
         except Exception as err:
             raise str(err)
 
